04df28696516e65fa8c16ce974397bbe4372f933
kayofujii <kayosamu1025@gmail.com>
2021-01-11 11:38:18 +0900

    編集・削除機能の実装

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -17,6 +17,26 @@ class GoalsController < ApplicationController
         end
     end
 
+    def edit
+        @goal = Goal.find(params[:id])
+    end
+
+    def update
+        @goal = Goal.find(params[:id])
+        @goal.assign_attributes goal_params
+        if @goal.save
+            redirect_to action: :index
+        else
+            redirect_to action: :edit
+        end
+    end
+
+    def destroy
+        @goal = Goal.find(params[:id])
+        @goal.destroy
+        redirect_to goals_url, notice: "目標を削除しました"
+    end
+
     private
     def goal_params
         params.require(:goal).permit(:goal_content,:action_content,:identity_content,:rank)
