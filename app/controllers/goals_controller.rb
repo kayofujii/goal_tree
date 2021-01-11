c647943e2bd068fc439734fd2eebd436a05604ce
kayofujii <kayosamu1025@gmail.com>
2021-01-11 10:36:17 +0900

    goalモデルの作成、indexとnewアクションの追加

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,2 +1,24 @@
 class GoalsController < ApplicationController
+    def index
+        @goals = Goal.all
+    end
+
+    def new
+        @goal = Goal.new
+    end
+
+    def create
+        @goal = Goal.new(goal_params)
+
+        if @goal.save
+            redirect_to action: :index
+        else
+            redirect_to action: :new
+        end
+    end
+
+    private
+    def goal_params
+        params.require(:goal).permit(:goal_content,:action_content,:identity_content,:rank)
+    end
 end
--- db/migrate/20210102011424_create_goals.rb
+++ db/migrate/20210102011424_create_goals.rb
@@ -2,10 +2,10 @@ class CreateGoals < ActiveRecord::Migration[6.1]
   def change
     create_table :goals do |t|
 
-      t.text :goal_content
-      t.text :action_content
-      t.text :identity_content
-      t.integer :rank
+      t.text :goal_content, null: false
+      t.text :action_content, null: false
+      t.text :identity_content, null: false
+      t.integer :rank, null: false, default: 0
 
       t.timestamps
     end
