44265e7340c45115a776d64c2a59c21160edd02b
kayofujii <kayosamu1025@gmail.com>
2021-01-16 12:29:02 +0900

    goalのカラムがnilのとき、エラー文が出るように。viewの共通化

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,2 +1,6 @@
 class Goal < ApplicationRecord
+    validates :goal_content, presence: true
+    validates :action_content, presence: true
+    validates :identity_content, presence: true
+    validates :rank, presence: true
 end
