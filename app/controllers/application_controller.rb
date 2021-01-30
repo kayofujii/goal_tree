e81b0dc86e032b03573752aa5aa493832c7a155f
kayofujii <kayosamu1025@gmail.com>
2021-01-30 13:03:49 +0900

    userコントローラーの作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
+    before_action :authenticate_user!
     before_action :configure_permitted_parameters, if: :devise_controller?
 
     protected
-
     def configure_permitted_parameters
         added_attrs = [ :email, :name, :description, :icon, :password, :password_confirmation ]
         devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  validates :description, length: { maximum: 200 }
+
   has_many :goals
 end
