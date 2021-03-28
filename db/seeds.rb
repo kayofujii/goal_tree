3a38fd45e2fb262429b2918ddf2a1ad56c863036
kayofujii <kayosamu1025@gmail.com>
2021-03-28 17:35:46 +0900

    seedの追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+%W[仕事 趣味 健康 勉強 お金 人間関係 家族 その他].each { |sample| GoalCategory.create(name: sample) }
