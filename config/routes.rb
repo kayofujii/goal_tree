68e2901f9dfa0d1231343aaf3916eb9c369c6bb2
kayofujii <kayosamu1025@gmail.com>
2021-04-23 21:27:46 +0900

    タイムラインにヘッダーを変更、細かい修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { 
     omniauth_callbacks: 'users/omniauth_callbacks', registrations: "users/registrations",
     passwords: "users/passwords"}
-  root to: 'goals#tumiage'
+  root to: 'goals#index'
   resources :goals, shallow: true do
     resources :action_records
     resources :goal_actions
