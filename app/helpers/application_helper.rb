012ad0d0ff5fc32e14bb373e7398f76a3367e827
kayofujii <kayosamu1025@gmail.com>
2021-05-04 16:31:26 +0900

    meta-tagsの設定2

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,24 +14,45 @@ module ApplicationHelper
 
     def default_meta_tags
         {
+            site: 'Tumiki ――挫折しない目標達成を支援するサービス',
+            title: 'Tumiki（つみき）',
             reverse: true,
             separator: '|',
             description: '挫折しない目標達成を支援するwebアプリです。使い方は3ステップで簡単。
                 海外大学で実証されている目標達成フレームワークに沿って目標を登録、
                 目標に向けた行動を記録して、ランクを上げていけば自然と目標達成ができます。',
             canonical: request.original_url,
-            noindex: ! Rails.env.production?
+            noindex: ! Rails.env.production?,
+            icon: [
+                { href: image_url('apple-touch-icon.png') },
+                { href: image_url('apple-touch-icon.png'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image' },
+            ],
+            og: {
+                site_name: 'Tumiki ――挫折しない目標達成を支援するサービス',
+                title: 'Tumiki（つみき）',
+                description: '挫折しない目標達成を支援するwebアプリです。使い方は3ステップで簡単。
+                    海外大学で実証されている目標達成フレームワークに沿って目標を登録、
+                    目標に向けた行動を記録して、ランクを上げていけば自然と目標達成ができます。', 
+                type: 'website',
+                url: request.original_url,
+                image: asset_url('apple-touch-icon.png'),
+                locale: 'ja_JP',
+            },
+            twitter: {
+                card: 'summary',
+                site: '@munchkayo1025',
+            }
         }
     end
     # metaタグ例
     # https://creat4869.hatenablog.com/entry/2019/08/15/170109
     # def default_meta_tags
     #     {
-    #       site: 'サイト名',
-    #       title: 'タイトル',
+    #       site: 'note ――つくる、つながる、とどける。',
+    #       title: 'note（ノート）',
     #       reverse: true,
     #       separator: '|',
-    #       description: 'ディスクリプション',
+    #       description: 'クリエイターが文章やマンガ、写真、音声を投稿することができ、ユーザーはそのコンテンツを楽しんで応援できるメディアプラットフォームです。だれもが創作を楽しんで続けられるよう、安心できる雰囲気や、多様性を大切にしています',
     #       keywords: 'キーワード',
     #       canonical: request.original_url,
     #       noindex: ! Rails.env.production?,
