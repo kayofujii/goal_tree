b3e7480fb2a1fb420f54f6beb03c982bd7abf816
kayofujii <kayosamu1025@gmail.com>
2021-05-07 21:38:33 +0900

    kaminari全画面実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
+        @goals = @user.goals.order(rank: "DESC").page(params[:goal_page])
+        @action_records = @user.action_records.order(id: "DESC").page(params[:action_record_page])
     end
 end
